7e62b9cfd63743d352b549656ca81a341cf140ff
Max Jöhnk <maxjoehnk@gmail.com>
2018-06-12 12:38:32 +0200

    [feat] cache and scale down coverart images

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,8 @@ fn main() -> Result<(), Error> {
         mpd::start(config.mpd.clone(), Arc::clone(&app)),
         http::start(config.http.clone(), Arc::clone(&app)),
         rustic::sync::start(Arc::clone(&app)),
-        rustic::player::start(Arc::clone(&app))
+        rustic::player::start(Arc::clone(&app)),
+        rustic::cache::start(Arc::clone(&app))?
     ];
 
     for handle in threads {
